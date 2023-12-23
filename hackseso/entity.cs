3ac62c98f81f988fd56df1d79d2045bf28b5a0ca
Thomas <viltetomas2003@gmail.com>
2023-12-23 04:37:26 -0300

    entity

---
+++ hackseso/entity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counter_Strike_2_Multi
+{
+    public class Entity
+    {
+        public IntPtr address { get; set; }
+
+        public int health { get; set; }
+
+        public Vector3 origin { get; set; }
+    }
+}
